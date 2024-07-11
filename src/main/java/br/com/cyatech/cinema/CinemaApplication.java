3f22229781a7a783aa8538fa42d76c546741cb82
cyajuninho <juninho225487@gmail.com>
2024-07-11 19:37:22 -0300

    fix: connecting to database via external file (final version)

--- src/main/java/br/com/cyatech/cinema/CinemaApplication.java
+++ src/main/java/br/com/cyatech/cinema/CinemaApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CinemaApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.jpa.hibernate.ddl-auto", "update");
 
         SpringApplication.run(CinemaApplication.class, args);
 	}
-
 }
