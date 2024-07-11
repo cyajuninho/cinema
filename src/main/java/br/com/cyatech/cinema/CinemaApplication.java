75dfb61f03efbd04e94524e4ac8f649bb7b0d9c9
cyajuninho <juninho225487@gmail.com>
2024-07-11 18:49:12 -0300

    fix: connecting to database via code

--- src/main/java/br/com/cyatech/cinema/CinemaApplication.java
+++ src/main/java/br/com/cyatech/cinema/CinemaApplication.java
@@ -2,18 +2,11 @@ package br.com.cyatech.cinema;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.PropertySource;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 @SpringBootApplication
-@PropertySource("file:c:/applications_properties/cinema_dev.properties.txt")
 public class CinemaApplication {
 
-	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+	public static void main(String[] args) {
 
         SpringApplication.run(CinemaApplication.class, args);
 	}
