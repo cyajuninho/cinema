86f74ebadbf70dac55fcc66fe6e770e2d30fcf67
cyajuninho <juninho225487@gmail.com>
2024-07-11 20:16:25 -0300

    feat: updateMovie in MovieService to find register before update

--- src/main/java/br/com/cyatech/cinema/service/MovieService.java
+++ src/main/java/br/com/cyatech/cinema/service/MovieService.java
@@ -5,6 +5,8 @@ import br.com.cyatech.cinema.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class MovieService {
 
@@ -43,10 +45,20 @@ public class MovieService {
 
     /**
      * Atualiza um filme
+     * @param id        Id do filme
      * @param movie     Filme a ser atualizado
      * @return Movie
      */
-    public Movie updateMovie(Movie movie) {
-        return movieRepository.save(movie);
+    public Movie updateMovie(Long id, Movie movie) {
+        Optional<Movie> existingMovie = movieRepository.findById(id);
+        if (existingMovie.isPresent()) {
+            Movie movieToUpdate = existingMovie.get();
+            movieToUpdate.setTitle(movie.getTitle());
+            movieToUpdate.setDescription(movie.getDescription());
+            movieToUpdate.setDuration(movie.getDuration());
+            movieToUpdate.setReleaseDate(movie.getReleaseDate());
+            return movieRepository.save(movie);
+        }
+        throw new IllegalArgumentException("Filme não encontrado");
     }
 }
