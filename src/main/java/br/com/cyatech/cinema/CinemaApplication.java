133eaee9c4c56fb0eec9ea5bf54f69e13e72c04f
cyajuninho <juninho225487@gmail.com>
2024-07-11 00:37:44 -0300

    fix: connecting to database via external file

--- src/main/java/br/com/cyatech/cinema/CinemaApplication.java
+++ src/main/java/br/com/cyatech/cinema/CinemaApplication.java
@@ -2,27 +2,19 @@ package br.com.cyatech.cinema;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.PropertySource;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 @SpringBootApplication
+@PropertySource("file:c:/applications_properties/cinema_dev.properties.txt")
 public class CinemaApplication {
 
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-		DatabaseConfigReader reader = new DatabaseConfigReader();
-		reader.readConfig("c:\\applications_properties\\cinema_dev.properties.txt");
-
-
-		try {
-			Connection connection = DriverManager.getConnection(reader.getDbUrl(), reader.getDbUser(), reader.getDbPassword());
-			System.out.println("Conexão estabelecida? " + !connection.isClosed());
-		} catch (SQLException e) {
-			System.out.println("Erro ao conectar ao banco de dados");
-			e.printStackTrace();
-		}
         SpringApplication.run(CinemaApplication.class, args);
 	}
 
